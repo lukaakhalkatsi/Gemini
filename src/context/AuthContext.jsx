470d8d4380d24475401ada26f5e10ab5bb5492b2
Luka <luka.akhalkatsi.1@btu.edu.ge>
2024-05-21 08:00:53 -0700

    auth state fix

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,10 @@
-import { createContext, useState } from "react";
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import React, { createContext, useState, useEffect } from "react";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase";
 import toast from "react-hot-toast";
 
@@ -13,6 +18,24 @@ const AuthContextProvider = ({ children }) => {
     prompt: "select_account",
   });
   let userRole;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(fbauth, (user) => {
+      if (user) {
+        userRole =
+          user.email === import.meta.env.VITE_LOGIN_EMAIL ? "Admin" : "User";
+        user.role = userRole;
+        setCurrentUser(user);
+        localStorage.setItem("userToken", user.accessToken);
+      } else {
+        setCurrentUser(null);
+        localStorage.removeItem("userToken");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   const handleGoogleSign = async (e) => {
     e.preventDefault();
     try {
@@ -25,12 +48,12 @@ const AuthContextProvider = ({ children }) => {
         userRole =
           user.email === import.meta.env.VITE_LOGIN_EMAIL ? "Admin" : "User";
         user.role = userRole;
-        console.log(user);
         setCurrentUser(user);
         localStorage.setItem("userToken", token);
       } else {
         toast.error("Something Went Wrong", {});
       }
+      console.log(user);
     } catch (error) {
       const errorCode = error.code;
       const errorMessage = error.message;
