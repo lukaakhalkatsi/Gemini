fbbf7397b404ca766f1b9d708c77b600c58f56ac
Luka <luka.akhalkatsi.1@btu.edu.ge>
2024-05-28 12:03:48 -0700

    protector tests

--- src/RouteProtector/Protector.jsx
+++ src/RouteProtector/Protector.jsx
@@ -1,11 +1,11 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+// import { useContext } from "react";
+// import { Navigate } from "react-router-dom";
+// import { AuthContext } from "../context/AuthContext";
 
-function Protector({ children }) {
-  const { currentUser } = useContext(AuthContext);
+// function Protector({ children }) {
+//   const { currentUser } = useContext(AuthContext);
 
-  return <>{currentUser ? children : <Navigate to="/" />}</>;
-}
+//   return <>{currentUser ? children : <Navigate to="/" />}</>;
+// }
 
-export default Protector;
+// export default Protector;
